19b055f611a30c879a0317080af6c6e17325e41c
Aylin Canpunar <aylin.canpunar@elev.nbi-handelsakademin.se>
2022-05-04 10:12:27 +0200

    aylin

---
+++ inlämninguppgifter/Inlämning1maj/style.cs
@@ -0,0 +1,12 @@
+a {
+    color: black;
+    text-decoration: none;
+}
+.div1{
+    grid-column: 1/3;
+    grid-row: 1/5;
+    width: 300px;
+    height: 200px;
+    border: 2px solid blue;
+    box-sizing: border-box;
+}
